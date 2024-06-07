d6265f49061ce4cb250ff2e7d718b2b5a4002872
kiranwannerkar <indiyago@gmail.com>
2024-06-07 12:22:43 +0530

    Spring batch Operation

---
+++ src/main/java/in/ashokit/config/CustomerProcessor.java
@@ -0,0 +1,20 @@
+package in.ashokit.config;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import in.ashokit.entity.Customer;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+
+	@Override
+	public Customer process(Customer item) throws Exception {
+
+	  if(item.getCountry().equals("China")) {
+		  return item;
+	  }
+
+		return null;
+	}
+
+}
+ 
